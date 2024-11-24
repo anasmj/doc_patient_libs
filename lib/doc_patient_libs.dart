20a970d773c9eb8e59789d15d86baeb9f38a2091
anas <anasmj7@gmail.com>
2024-11-24 23:45:02 +0600

    added new service

--- lib/doc_patient_libs.dart
+++ lib/doc_patient_libs.dart
@@ -3,3 +3,4 @@ library doc_patient_libs;
 export '../src/models/models.dart';
 export './src/services/cehmber.service/chember.service.dart';
 export './src/services/appointment.service/appointment.service.dart';
+export './src/services/team.service/team.service.dart';
