a3a801ecb6e9493cb37e550fb803ba4de244aca3
anas <anasmj7@gmail.com>
2024-11-28 00:20:42 +0600

    updated

--- lib/doc_patient_libs.dart
+++ lib/doc_patient_libs.dart
@@ -4,3 +4,4 @@ export '../src/models/models.dart';
 export './src/services/cehmber.service/chember.service.dart';
 export './src/services/appointment.service/appointment.service.dart';
 export './src/services/service.collections.dart';
+export './src/services/frebase.crud.dart';
