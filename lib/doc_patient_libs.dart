2d63db50a022bef1c0f7b3d081a1b5ee8dd490ac
anas <anasmj7@gmail.com>
2024-11-28 00:17:26 +0600

    removed team service

--- lib/doc_patient_libs.dart
+++ lib/doc_patient_libs.dart
@@ -3,5 +3,4 @@ library doc_patient_libs;
 export '../src/models/models.dart';
 export './src/services/cehmber.service/chember.service.dart';
 export './src/services/appointment.service/appointment.service.dart';
-export './src/services/team.service/team.service.dart';
 export './src/services/service.collections.dart';
