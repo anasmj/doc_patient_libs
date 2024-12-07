5fb8b3fbb0410896f4397d28bf8e390771ea7aff
anas <anasmj7@gmail.com>
2024-12-07 10:36:31 +0600

    two filds added to chember

--- lib/src/models/src/chember.dart
+++ lib/src/models/src/chember.dart
@@ -23,19 +23,22 @@ class Chember {
   final double? lat;
   final double? long;
   final List<Schedule>? schedules;
+  final int? firstVisitFee;
+  final int? followupFee;
 
-  const Chember({
-    this.id,
-    this.name,
-    this.address,
-    this.roomNo,
-    this.chemberEmail,
-    this.phone,
-    this.floor,
-    this.lat,
-    this.long,
-    this.schedules,
-  });
+  const Chember(
+      {this.id,
+      this.name,
+      this.address,
+      this.roomNo,
+      this.chemberEmail,
+      this.phone,
+      this.floor,
+      this.lat,
+      this.long,
+      this.schedules,
+      this.firstVisitFee,
+      this.followupFee});
 
   Chember copyWith({
     String? id,
@@ -48,20 +51,23 @@ class Chember {
     String? web,
     double? lat,
     double? long,
+    int? firstVisitFee,
+    int? followupFee,
     List<Schedule>? schedules,
   }) {
     return Chember(
-      id: id ?? this.id,
-      name: name ?? this.name,
-      address: address ?? this.address,
-      roomNo: roomNo ?? this.roomNo,
-      chemberEmail: chemberEmail ?? this.chemberEmail,
-      phone: phone ?? this.phone,
-      floor: floor ?? this.floor,
-      lat: lat ?? this.lat,
-      long: long ?? this.long,
-      schedules: schedules ?? this.schedules,
-    );
+        id: id ?? this.id,
+        name: name ?? this.name,
+        address: address ?? this.address,
+        roomNo: roomNo ?? this.roomNo,
+        chemberEmail: chemberEmail ?? this.chemberEmail,
+        phone: phone ?? this.phone,
+        floor: floor ?? this.floor,
+        lat: lat ?? this.lat,
+        long: long ?? this.long,
+        schedules: schedules ?? this.schedules,
+        firstVisitFee: firstVisitFee ?? this.firstVisitFee,
+        followupFee: followupFee ?? this.followupFee);
   }
 
   Map<String, dynamic> toMap() {
@@ -76,30 +82,35 @@ class Chember {
       'lat': lat,
       'long': long,
       'schedules': schedules?.map((x) => x.toMap()).toList(),
+      'firstVisitFee': firstVisitFee,
+      'followupFee': followupFee
     };
   }
 
   factory Chember.fromMap(Map<String, dynamic> map) {
     return Chember(
-      id: map['id'] != null ? map['id'] as String : null,
-      name: map['name'] != null ? map['name'] as String : null,
-      address: map['address'] != null ? map['address'] as String : null,
-      roomNo: map['roomNo'] != null ? map['roomNo'] as String : null,
-      chemberEmail:
-          map['chemberEmail'] != null ? map['chemberEmail'] as String : null,
-      phone: map['phone'] != null ? map['phone'] as String : null,
-      // phone: map['phone'] != null
-      //     ? List<String>.from((map['phone'] as List<String>))
-      //     : null,
-      floor: map['floor'] != null ? map['floor'] as String : null,
-      lat: map['lat'] != null ? map['lat'] as double : null,
-      long: map['long'] != null ? map['long'] as double : null,
-      schedules: map['schedules'] != null
-          ? (map['schedules'] as List)
-              .map((scheduleData) => Schedule.fromMap(scheduleData))
-              .toList()
-          : null,
-    );
+        id: map['id'] != null ? map['id'] as String : null,
+        name: map['name'] != null ? map['name'] as String : null,
+        address: map['address'] != null ? map['address'] as String : null,
+        roomNo: map['roomNo'] != null ? map['roomNo'] as String : null,
+        chemberEmail:
+            map['chemberEmail'] != null ? map['chemberEmail'] as String : null,
+        phone: map['phone'] != null ? map['phone'] as String : null,
+        // phone: map['phone'] != null
+        //     ? List<String>.from((map['phone'] as List<String>))
+        //     : null,
+        floor: map['floor'] != null ? map['floor'] as String : null,
+        lat: map['lat'] != null ? map['lat'] as double : null,
+        long: map['long'] != null ? map['long'] as double : null,
+        schedules: map['schedules'] != null
+            ? (map['schedules'] as List)
+                .map((scheduleData) => Schedule.fromMap(scheduleData))
+                .toList()
+            : null,
+        firstVisitFee:
+            map['firstVisitFee'] != null ? map['firstVisitFee'] as int : null,
+        followupFee:
+            map['followupFee'] != null ? map['followupFee'] as int : null);
   }
 
   String toJson() => json.encode(toMap());
@@ -109,7 +120,7 @@ class Chember {
 
   @override
   String toString() {
-    return 'Chember(name: $name, address: $address, roomNo: $roomNo, chemberEmail: $chemberEmail, phone: $phone, floor: $floor, lat: $lat, long: $long, schedules: $schedules)';
+    return 'Chember(name: $name, address: $address, roomNo: $roomNo, chemberEmail: $chemberEmail, phone: $phone, floor: $floor, lat: $lat, long: $long, schedules: $schedules, firstVisitFee: $firstVisitFee, followupFee: $followupFee)';
   }
 
   @override
@@ -126,6 +137,8 @@ class Chember {
         other.floor == floor &&
         other.lat == lat &&
         other.long == long &&
+        other.firstVisitFee == firstVisitFee &&
+        other.followupFee == followupFee &&
         listEquals(other.schedules, schedules);
   }
 
@@ -140,6 +153,8 @@ class Chember {
         floor.hashCode ^
         lat.hashCode ^
         long.hashCode ^
+        firstVisitFee.hashCode ^
+        followupFee.hashCode ^
         schedules.hashCode;
   }
 }
