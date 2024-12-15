6bf909777d60d1f67610fe9a568788e2d7323c6f
anas <anasmj7@gmail.com>
2024-12-16 00:24:18 +0600

    update

--- lib/src/models/models.dart
+++ lib/src/models/models.dart
@@ -1,18 +1,16 @@
 import 'dart:convert';
 
+import 'package:doc_patient_libs/src/extensions/extensions.dart';
 import 'package:doc_patient_libs/src/models/src/profile.type.dart';
 import 'package:enum_to_string/enum_to_string.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:doc_patient_libs/src/extensions/extensions.dart';
-import 'package:uuid/uuid.dart';
 
 part '../models/src/appointment.dart';
 part '../models/src/chember.dart';
+part '../models/src/enums.dart';
 part '../models/src/patient.data.dart';
+part '../models/src/profile.dart';
 part '../models/src/schedule.dart';
 part '../models/src/slot.dart';
-part '../models/src/enums.dart';
-
-part '../models/src/profile.dart';
 part 'src/user.device.dart';
