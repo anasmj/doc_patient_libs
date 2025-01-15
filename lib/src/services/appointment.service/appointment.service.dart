99846899f6fc551df8995369b7821004de601ac0
anas <anasmj7@gmail.com>
2025-01-15 21:43:26 +0600

    update

--- lib/src/services/appointment.service/appointment.service.dart
+++ lib/src/services/appointment.service/appointment.service.dart
@@ -11,12 +11,11 @@ class AppointmentService {
   static const approved = 'approved_appointments';
   static const past = 'past_appointments';
   // Create a new appointment
-  static Future<void> create(String collectionName, String appointmentId,
-      Map<String, dynamic> data) async {
+  static Future<void> create(
+      String appointmentId, Map<String, dynamic> data) async {
     try {
-      await _firestore.collection(collectionName).doc(appointmentId).set(data);
-      debugPrint(
-          'Appointment created successfully in $collectionName collection.');
+      await _firestore.collection(reqeusted).doc(appointmentId).set(data);
+      debugPrint('Appointment created successfully.');
     } catch (e) {
       debugPrint('Error creating appointment: $e');
     }
@@ -127,67 +126,3 @@ class AppointmentService {
     }
   }
 }
-
-// class AppointmentService extends BaseService<Appointment> {
-//   AppointmentService._();
-
-//   static final AppointmentService instance = AppointmentService._();
-
-//   final CollectionReference _collection =
-//       FirebaseFirestore.instance.collection(ServiceCollection.appointments);
-
-//   @override
-//   Future<String?> create(Appointment data) async {
-//     try {
-//       DocumentReference docRef = await _collection.add(data.toMap());
-//       return docRef.id;
-//     } catch (e) {
-//       rethrow;
-//     }
-//   }
-
-//   @override
-//   Future<Appointment?> getSingle(String id) async {
-//     try {
-//       DocumentSnapshot doc = await _collection.doc(id).get();
-//       final data = doc.exists ? doc.data() as RawData : null;
-//       if (data == null) return null;
-//       return Appointment.fromMap(data);
-//     } catch (e) {
-//       rethrow;
-//     }
-//   }
-
-//   @override
-//   Future<List<Appointment>> getAll() async {
-//     try {
-//       QuerySnapshot querySnapshot = await _collection.get();
-//       final datas =
-//           querySnapshot.docs.map((doc) => doc.data() as RawData).toList();
-//       return datas.map((data) => Appointment.fromMap(data)).toList();
-//     } catch (e) {
-//       rethrow;
-//     }
-//   }
-
-//   @override
-//   Future<bool> update(String id, Appointment data) async {
-//     try {
-//       await _collection.doc(id).update(data.toMap());
-//       return true;
-//     } catch (e) {
-//       rethrow;
-//     }
-//   }
-
-//   @override
-//   Future<bool> delete(String id) async {
-//     try {
-//       await _collection.doc(id).delete();
-//       return true;
-//     } catch (e) {
-//       // print("Error deleting document: $e");
-//       rethrow;
-//     }
-//   }
-// }
