0591dffcedaf9024526072378f0e439966751156
anas <anasmj7@gmail.com>
2025-01-22 23:16:53 +0600

    add id to slot

--- lib/src/models/src/slot.dart
+++ lib/src/models/src/slot.dart
@@ -2,15 +2,20 @@
 part of '../models.dart';
 
 class Slot {
+  ///iekif3-erjikj-kjrkej-ilkdf_1
+  ///scheduleId_index
+  final String? id;
   final int? index;
   final TimeOfDay? start;
-  Slot({this.index, this.start});
+  Slot({this.id, this.index, this.start});
 
   Slot copyWith({
+    String? id,
     int? index,
     TimeOfDay? start,
   }) {
     return Slot(
+      id: id ?? this.id,
       index: index ?? this.index,
       start: start ?? this.start,
     );
@@ -18,6 +23,7 @@ class Slot {
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
+      'id': id,
       'index': index,
       'start': start != null ? _timeOfDayToString(start!) : null,
     };
@@ -25,6 +31,7 @@ class Slot {
 
   factory Slot.fromMap(Map<String, dynamic> map) {
     return Slot(
+      id: map['id'] != null ? map['id'] as String : null,
       index: map['index'] != null ? map['index'] as int : null,
       start: _stringToTimeOfDay(map['start'] as String),
     );
@@ -36,14 +43,14 @@ class Slot {
       Slot.fromMap(json.decode(source) as Map<String, dynamic>);
 
   @override
-  String toString() => 'Slot(index: $index,  Slot(start: $start)';
+  String toString() => 'Slot(id: $id, index: $index,  Slot(start: $start)';
 
   @override
   bool operator ==(covariant Slot other) {
     if (identical(this, other)) return true;
-    return other.index == index && other.start == start;
+    return other.id == id && other.index == index && other.start == start;
   }
 
   @override
-  int get hashCode => index.hashCode ^ start.hashCode;
+  int get hashCode => id.hashCode ^ index.hashCode ^ start.hashCode;
 }
